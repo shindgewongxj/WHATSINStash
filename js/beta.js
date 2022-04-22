88a18ba63946114ace0a163d1ff1186f79316602
$H!NDGEKYUME <59656935+shindgewongxj@users.noreply.github.com>
2022-04-22 20:33:51 +0800

    Update beta.js

--- js/beta.js
+++ js/beta.js
@@ -61,7 +61,7 @@ let { body } = $response;
                     // TODO: compare yestarday AQI
                 );
 
-                // $.log(`🚧 ${$.name}, type: ${returnedData}`, "");
+                $.log(`🚧 ${$.name}, type: ${JSON.stringify(returnedData)}`, "");
 
                 // Empty for URL
                 var Station = { now: { country: "" } };
