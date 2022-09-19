c407b9beae925a7fcfae50514e0a8a3cf3ea0f4f
shindgewongxj <shindgewongxj@whatsin.vip>
2022-09-19 11:06:44 +0800

    Fixed syntax.

--- javascript/ipgeolocation.js
+++ javascript/ipgeolocation.js
@@ -1,14 +1,15 @@
 $httpClient.get("http://ip-api.com/json", function(error, response, data) {
-  let jsonData = JSON.parse(data)
-  let country = jsonData.country
-  let state = jsonData.regionName
-  let city = jsonData.city
-  let isp = jsonData.isp
-  let ip = jsonData.query
-  body = {
-    title: `${country}`,
-    content: `${state}\n${city}\n${isp}`,
-    backgroundColor: "#737373",
-    icon: "location",
-  }$done(body);
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let state = jsonData.regionName
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+    body = {
+        backgroundColor: "#737373",
+        icon: "location",
+        title: `${country}`,
+        content: `${state}\n${city}\n${isp}`,
+    }
+    $done(body);
 });
