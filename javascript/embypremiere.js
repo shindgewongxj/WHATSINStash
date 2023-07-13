b8db4153e9a92a9a6c5f2cdb6939d6ced10431ac
$H!NDGEKYUME <shindgewongxj@whatsin.ltd>
2023-07-13 19:58:42 +0800

    Modified notification text.

--- javascript/embypremiere.js
+++ javascript/embypremiere.js
@@ -1,10 +1,14 @@
 if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
-    if($response.status!=200){
-        $notification.post("Emby Premiere Unlocked", "", "");
-        $done({status: 200, headers: $response.headers, body: '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}' })
-    }else{
+    if ($response.status != 200) {
+        $notification.post("Emby Premiere Unlocked!");
+        $done({
+            status: 200,
+            headers: $response.headers,
+            body: '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}'
+        })
+    } else {
         $done({})
     }
-}else{
+} else {
     $done({})
 }
